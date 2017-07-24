006549ce537b4f43205f0ca0c45800fdfa82663d
JaganNarayanan <jns405@nyu.edu>
2017-07-24 16:09:33 +0800

    Completed.

--- pig-latin.js
+++ pig-latin.js
@@ -2,12 +2,30 @@ function getPigLatin() {
 
   //get the word input by the user
   var str = document.getElementById("word").value;
-  
 
   //your code goes here
+  var storage = "";
+  if (str[0] === "a" || str[0] === "e" || str[0] === "i" || str[0] === "o" || str[0] === "u") {
+    str = str;
+  }
+  else {
+    storage += str[0];
+    str = str.substring(1);
+    while (true) {
+      if (str[0] === "a" || str[0] === "e" || str[0] === "i" || str[0] === "o" || str[0] === "u") {
+        str = str;
+        break;
+      }
+      else {
+        storage += str[0];
+        str = str.substring(1);
+      }
+    }
+  }
+  new_str = str + storage + "ay";
 
-  //change the HTML content of a <p> element with id="pig-latin-word": 
-  document.getElementById("pig-latin-word").innerHTML = str;
+  //change the HTML content of a <p> element with id="pig-latin-word":
+  document.getElementById("pig-latin-word").innerHTML = new_str;
 
 } //end of function getPigLatin()
 
